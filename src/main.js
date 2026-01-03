e7769f7aa3170ac4ec4b8cc579de8b65a41f6fb7
SaraMsoso <Sara.soder44@gmail.com>
2026-01-03 21:51:39 +0100

    Initialized Vue app with Pinia, router, and global styles

--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import { router } from "./router/index.js";
 
-import App from './App.vue'
-import router from './router'
+import "./style.css";
 
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+createApp(App)
+  .use(createPinia())
+  .use(router)
+  .mount("#app");
