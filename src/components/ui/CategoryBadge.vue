6a247cf06e5f198e830ba47f3b9ca85477aa4eb1
SaraMsoso <Sara.soder44@gmail.com>
2026-01-05 09:20:49 +0100

    Removed same class bg-brand-light

--- src/components/ui/CategoryBadge.vue
+++ src/components/ui/CategoryBadge.vue
@@ -1,7 +1,7 @@
 <!-- Small badge for category labels -->
 <template>
   <span
-    class="inline-flex items-center px-3 py-1 rounded-full bg-brand-light text-brand-dark text-xs font-medium bg-(--color-light)"
+    class="inline-flex items-center px-3 py-1 rounded-full text-brand-dark text-xs font-medium bg-(--color-light)"
   >
     <slot />
   </span>
